de1436ab4b0c57e943b6e902a276e3c2d36a12c6
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-07 12:06:26 -0700

    red test for displaying zodiac detail page

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,9 +13,20 @@ describe('zodiac-API routes', () => {
   it('/zodiacs should return a list of zodiacs', async () => {
     const resData = await request(app).get('/zodiacs');
     const expected = zodiacs.map((zodiac) => ({ id: zodiac.id, name: zodiac.name }));
-
     expect(resData.body).toEqual(expected);
   });
+
+  it('/zodiacs/:id should return zodiac detail', async () => {
+    const resData = await request(app).get('/zodiacs/2');
+    const aries = {
+      id: '2',
+      name: 'aries',
+      dates: 'Mar 21 - Apr 19',
+      symbol: 'Ram',
+    };
+    expect(resData.body).toEqual(aries);
+  });
+
 
   afterAll(() => {
     pool.end();
